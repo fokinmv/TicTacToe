ba12de6a48c36a6d526a269f070fcfd0e94f55a0
Mihail Fokin <fmv87@mail.ru>
2017-10-17 11:59:43 +0300

    add const indentation

--- app/shared/paint.service.ts
+++ app/shared/paint.service.ts
@@ -1,8 +1,12 @@
 import { Game } from './game.class';
 
 export class PaintService {
-    //размер ячейки игрового поля
+
     cellSize = 50; 
+    roleX : string = "X";
+    role0 : string = "0";
+    indentation : number = 0.25;
+
     private canvas: any;
 
     drawGameTable( canvas : any, gameSize : number ) {
@@ -15,10 +19,10 @@ export class PaintService {
         
                 this.canvas.width = gameSize*this.cellSize + 1;
                 this.canvas.height = gameSize*this.cellSize + 1;
-                //рисуем большую рамку
+
                 let widthTable: number = gameSize*this.cellSize;
                 ctx.strokeRect(0,0,widthTable,widthTable);
-                //рисуем ячейки
+
                 for (let i: number = 0; i < gameSize; i++)
                     for (let j: number = 0; j < gameSize; j ++) {
                         ctx.strokeRect(1 + j*this.cellSize, 1 + i*this.cellSize, this.cellSize, this.cellSize);
@@ -32,15 +36,15 @@ export class PaintService {
         let centerCellX : number = this.defineCellCenter(coordinateCellX, this.cellSize);
         let centerCellY : number = this.defineCellCenter(coordinateCellY, this.cellSize);
 
-        let line1Start1 = centerCellX-0.25*this.cellSize;
-        let line1Start2 = centerCellY-0.25*this.cellSize;
-        let line1Finish1 = centerCellX+0.25*this.cellSize;        
-        let line1Finish2 = centerCellY+0.25*this.cellSize;
+        let line1Start1 = centerCellX-this.indentation*this.cellSize;
+        let line1Start2 = centerCellY-this.indentation*this.cellSize;
+        let line1Finish1 = centerCellX+this.indentation*this.cellSize;        
+        let line1Finish2 = centerCellY+this.indentation*this.cellSize;
 
-        let line2Start1 = centerCellX-0.25*this.cellSize;
-        let line2Start2 = centerCellY+0.25*this.cellSize;
-        let line2Finish1 = centerCellX+0.25*this.cellSize;
-        let line2Finish2 = centerCellY-0.25*this.cellSize;
+        let line2Start1 = centerCellX-this.indentation*this.cellSize;
+        let line2Start2 = centerCellY+this.indentation*this.cellSize;
+        let line2Finish1 = centerCellX+this.indentation*this.cellSize;
+        let line2Finish2 = centerCellY-this.indentation*this.cellSize;
         
         if (this.canvas.getContext) {
             let canvas = this.canvas;
@@ -59,11 +63,11 @@ export class PaintService {
         }
     }
 
-    draw0 ( canvas : any, coordinateCellX : number, coordinateCellY : number) {
+    draw0 (canvas : any, coordinateCellX : number, coordinateCellY : number) {
         let centerCellX = this.defineCellCenter(coordinateCellX, this.cellSize);
         let centerCellY = this.defineCellCenter(coordinateCellY, this.cellSize);        
 
-        var radiusO = this.cellSize/4;
+        var radiusO = this.cellSize*this.indentation;
 
         if (this.canvas.getContext) {
             let canvas = this.canvas;
@@ -77,16 +81,16 @@ export class PaintService {
         }
     }
 
-    drawX0( canvas : any, value : any, size : number){
+    drawX0(canvas : any, value : any, size : number){
         for(var i=0; i<size; i++){
             for(var j=0; j<size; j++){
-                if (value[i][j] == 'X') this.drawX(canvas,j,i);
-                if (value[i][j] == '0') this.draw0(canvas,j,i);
+                if (value[i][j] == this.roleX) this.drawX(canvas,j,i);
+                if (value[i][j] == this.role0) this.draw0(canvas,j,i);
              };
          }
     };
 
-    defineCellCenter( cellNumber : number, cellSize : number) {
+    defineCellCenter(cellNumber : number, cellSize : number) {
         let cellCenter = (cellNumber+0.5)*cellSize;
         return cellCenter;
     }
