3d2dad9455fa45b452f9b914a4702861c78a3b5c
Mihail Fokin <fmv87@mail.ru>
2017-10-18 19:20:11 +0300

    add gulp compile ts/test2

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,9 +6,20 @@ const rename = require("gulp-rename");
 const minifyCSS = require("gulp-minify-css");
 const htmlmin = require("gulp-htmlmin");
 const del = require("del");
-const typescript = require("gulp-typescript");
+
+const tsc  = require('gulp-typescript-compiler');
+
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+
+const tscConfig = require('./tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
+
 const tslint = require('gulp-tslint');
+
+const browserify = require("browserify");
+const source = require('vinyl-source-stream');
+const tsify = require("tsify");
 
 gulp.task('clean', function () {
     return del('build/*');
@@ -28,13 +39,14 @@ gulp.task('build-html', function () {
         .pipe(gulp.dest('build/'));
 });
 
-gulp.task('compile', function () {
-    return gulp.src(['app/**/*.ts','!node_modules/**/*.ts'])
-        .pipe(typescript({
-            noImplicitAny: true,
-            outFile: 'output.js'
-        }))
-        .pipe(gulp.dest('built/'));
+gulp.task('compile', () => {
+    let tsResult = gulp
+        .src("app/**/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+    return tsResult.js
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("build/"));
 });
 
 gulp.task('default', ['clean','build-html','build-css','compile'], () => {
